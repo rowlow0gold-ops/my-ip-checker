ff47aa9ad7c06fbcb1008bffb5166598f6aeb31f
rowlow0gold-ops <rowlow0gold@gmail.com>
2026-04-07 12:37:21 +0900

    feat: add Google AdSense verification script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,13 @@ export default function RootLayout({
       lang="ko"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9779896074298699"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
